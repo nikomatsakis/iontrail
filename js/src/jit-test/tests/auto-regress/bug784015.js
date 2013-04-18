572c0402a3d321e8fb00f67c3711683b0e7c2861
Niko Matsakis <niko@alum.mit.edu>
2013-04-18 05:53:53 -0400

    Bug 861705 - Detect when ParallelArray is disabled in auto-regress tests

--- js/src/jit-test/tests/auto-regress/bug784015.js
+++ js/src/jit-test/tests/auto-regress/bug784015.js
@@ -4,5 +4,7 @@
 // Flags:
 //
 
-var p = new ParallelArray([1,25e8 ,3,4]);
-var pp = p.partition(.34 );
+if (getBuildConfiguration().parallelJS) {
+  var p = new ParallelArray([1,25e8 ,3,4]);
+  var pp = p.partition(.34 );
+}

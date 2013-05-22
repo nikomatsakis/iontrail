4688f407b894ec68736143de20b2911bf3be10e2
Niko Matsakis <niko@alum.mit.edu>
2013-05-22 07:01:46 -0400

    UnsafeGetImmutableElement: Add intrinsic and inline code, use for shape accesses

--- js/src/ion/MIR.h
+++ js/src/ion/MIR.h
@@ -4443,11 +4443,13 @@ class MLoadElement
 {
     bool needsHoleCheck_;
     bool loadDoubles_;
+    bool knownImmutable_; // load of data that is known to be immutable
 
-    MLoadElement(MDefinition *elements, MDefinition *index, bool needsHoleCheck, bool loadDoubles)
+    MLoadElement(MDefinition *elements, MDefinition *index, bool needsHoleCheck, bool loadDoubles, bool knownImmutable)
       : MBinaryInstruction(elements, index),
         needsHoleCheck_(needsHoleCheck),
-        loadDoubles_(loadDoubles)
+        loadDoubles_(loadDoubles),
+        knownImmutable_(knownImmutable)
     {
         setResultType(MIRType_Value);
         setMovable();
@@ -4459,8 +4461,10 @@ class MLoadElement
     INSTRUCTION_HEADER(LoadElement)
 
     static MLoadElement *New(MDefinition *elements, MDefinition *index,
-                             bool needsHoleCheck, bool loadDoubles) {
-        return new MLoadElement(elements, index, needsHoleCheck, loadDoubles);
+                             bool needsHoleCheck, bool loadDoubles,
+                             bool knownImmutable) {
+        return new MLoadElement(elements, index, needsHoleCheck, loadDoubles,
+                                knownImmutable);
     }
 
     TypePolicy *typePolicy() {
@@ -4482,6 +4486,9 @@ class MLoadElement
         return needsHoleCheck();
     }
     AliasSet getAliasSet() const {
+        if (knownImmutable_)
+            return AliasSet::None();
+
         return AliasSet::Load(AliasSet::Element);
     }
 };

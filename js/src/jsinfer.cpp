d88f76a031a0433d5508b2897f4eee77fa7fedc1
Niko Matsakis <niko@alum.mit.edu>
2013-08-16 17:32:58 -0400

    Bug898347-Part6-BlockAddenda Allow addenda to be added to binary blocks

--- js/src/jsinfer.cpp
+++ js/src/jsinfer.cpp
@@ -5236,6 +5236,7 @@ CheckNewScriptProperties(JSContext *cx, HandleTypeObject type, HandleFunction fu
 #else
     newScript = (TypeNewScript *) cx->calloc_(numBytes);
 #endif
+    new (newScript) TypeNewScript();
     type->addendum = newScript;
 
     if (!newScript) {
@@ -6774,7 +6775,15 @@ TypeZone::sweep(FreeOp *fop, bool releaseTypes)
 bool
 TypeObject::addBinaryDataAddendum(JSContext *cx, TypeRepresentation *repr)
 {
+    if (!cx->typeInferenceEnabled())
+        return true;
+
     JS_ASSERT(repr);
+
+    if (flags & OBJECT_FLAG_ADDENDUM_CLEARED)
+        return true;
+
+    JS_ASSERT(!unknownProperties());
 
     if (addendum) {
         JS_ASSERT(hasBinaryData());
@@ -6789,7 +6798,20 @@ TypeObject::addBinaryDataAddendum(JSContext *cx, TypeRepresentation *repr)
     return true;
 }
 
+/////////////////////////////////////////////////////////////////////
+// Type object addenda constructor
+/////////////////////////////////////////////////////////////////////
+
+TypeObjectAddendum::TypeObjectAddendum(Kind kind)
+  : kind(kind)
+{}
+
+TypeNewScript::TypeNewScript()
+  : TypeObjectAddendum(NewScript)
+{}
+
 TypeBinaryData::TypeBinaryData(TypeRepresentation *repr)
-  : typeRepr(repr)
+  : TypeObjectAddendum(BinaryData),
+    typeRepr(repr)
 {
 }

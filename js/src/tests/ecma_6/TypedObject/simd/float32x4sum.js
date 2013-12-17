678a4e3807386654fb08f09d5b3b3643b67e77bf
Haitao Feng <haitao.feng@intel.com>
2013-12-17 21:46:01 +0800

    Add test case

---
+++ js/src/tests/ecma_6/TypedObject/simd/float32x4sum.js
@@ -0,0 +1,23 @@
+var { float32x4 } = SIMD;
+var Data = float32x4.array(100);
+
+// instruct ion to kick in fast
+setJitCompilerOption("ion.usecount.trigger", 30);
+
+function sum(a) {
+  var sum = float32x4.zero();
+  for (var i = 0; i < a.length; i++) {
+    sum = SIMD.float32x4.add(sum, a[i]);
+  }
+  return sum;
+}
+
+function main() {
+  var a = new Data();
+  sum(a); //warmup
+  sum(a); //for reals
+  if (typeof reportCompare === "function")
+    reportCompare(true, true);
+}
+
+main();
